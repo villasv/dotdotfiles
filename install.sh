e12d4aaf4baed33aa95f869c5bb4b073b974c1f5
Victor Villas <villasv@outlook.com>
2018-06-21 16:09:03 +0000

    Add installer helpers for github releases

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+lrm () { # latest release matching
+  curl -L --silent "https://api.github.com/repos/$1/releases/latest" | jq -r '.assets[].browser_download_url' | grep $2
+}
+lrm_deb () {
+  PNAME=${1##*/}
+  PRURL=$(lrm $1 deb)
+  RFILE=${PRURL##*/}
+
+  if [ ! -f ~/.dotfiles/$RFILE.deb ];
+  then
+    echo Installing latest $PNAME: $RFILE
+    curl -L $PRURL --output ~/.dotfiles/$RFILE.deb
+    sudo dpkg -i ~/.dotfiles/$RFILE.deb
+  else
+    echo Latest $PNAME already installed
+  fi
+}
+
+lrm_deb BurntSushi/ripgrep
+
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 
