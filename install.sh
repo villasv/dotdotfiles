720d535263b533af5d4c4bada0635388cc88739f
Victor Villas <villasv@outlook.com>
2018-06-21 18:28:39 +0000

    Add fzf installer from github tarball

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-lrm () { # latest release matching
-  curl -L --silent "https://api.github.com/repos/$1/releases/latest" | jq -r '.assets[].browser_download_url' | grep $2
+lrp () { # latest release matching
+  curl -L --silent "https://api.github.com/repos/$1/releases/latest" | jq -r '.assets[].browser_download_url' | grep deb
 }
-lrm_deb () {
+lrp_dpkg () {
   PNAME=${1##*/}
-  PRURL=$(lrm $1 deb)
+  PRURL=$(lrp $1)
   RFILE=${PRURL##*/}
 
   if [ ! -f ~/.dotfiles/$RFILE.lock ];
@@ -16,11 +16,33 @@ lrm_deb () {
       && touch ~/.dotfiles/$RFILE.lock \
       && rm $RFILE
   else
-    echo Latest $PNAME already installed
+    echo "$PNAME already the newest version ($RFILE)."
   fi
 }
+lrp_dpkg BurntSushi/ripgrep
 
-lrm_deb BurntSushi/ripgrep
+ltb () { # latest tag tarball
+  curl -L --silent "https://api.github.com/repos/$1/tags" | jq -r '.[0].tarball_url'
+}
+ltb_install () {
+  PNAME=${1##*/}
+  PRURL=$(ltb $1)
+  RFILE=${PRURL##*/}
+
+  if [ ! -f ~/.dotfiles/$PNAME_$RFILE.lock ];
+  then
+    echo Installing latest $PNAME: $RFILE
+    mkdir -p ~/.fzf \
+      && curl -L $PRURL | tar -xz -C ~/.$PNAME --strip-components=1 \
+      && ~/.$PNAME/install \
+      && touch ~/.dotfiles/$PNAME_$RFILE.lock
+  else
+    echo "$PNAME already the newest version ($RFILE)."
+  fi
+}
+ltb_install junegunn/fzf
+
+sudo apt -qq install jq
 
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
