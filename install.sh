2175e4d4b4a8ff9fb6276206b8b8f33c2d6e737f
Victor Villas <villasv@outlook.com>
2018-06-21 16:17:03 +0000

    Add lockfile installation check instead of pkgs

--- install.sh
+++ install.sh
@@ -8,11 +8,13 @@ lrm_deb () {
   PRURL=$(lrm $1 deb)
   RFILE=${PRURL##*/}
 
-  if [ ! -f ~/.dotfiles/$RFILE.deb ];
+  if [ ! -f ~/.dotfiles/$RFILE.lock ];
   then
     echo Installing latest $PNAME: $RFILE
-    curl -L $PRURL --output ~/.dotfiles/$RFILE.deb
-    sudo dpkg -i ~/.dotfiles/$RFILE.deb
+    curl -L $PRURL --output ~/.dotfiles/$RFILE
+    sudo dpkg -i ~/.dotfiles/$RFILE \
+      && touch ~/.dotfiles/$RFILE.lock \
+      && rm $RFILE
   else
     echo Latest $PNAME already installed
   fi
