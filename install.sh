4e9f6beb62e176c56863332b3f16f27958f9c618
Victor Villas <mail@victor.villas>
2019-11-28 12:03:12 -0300

    Add bootstrap directory section

--- install.sh
+++ install.sh
@@ -1,53 +1,77 @@
 #!/bin/bash
 
-sudo apt -qq install jq zsh
+DOTS="$HOME/.dotfiles"
 
-# Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-lrp () { # latest release matching
-  curl -L --silent "https://api.github.com/repos/$1/releases/latest" | jq -r '.assets[].browser_download_url' | grep deb
-}
-lrp_dpkg () {
-  PNAME=${1##*/}
-  PRURL=$(lrp $1)
-  RFILE=${PRURL##*/}
-
-  if [ ! -f ~/.dotfiles/${PNAME}_${RFILE}.lock ];
+# Bootstrap Dotfiles
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ "$CURR_DIR" != "$DOTS" ]
+then
+  echo "Project directory at $CURR_DIR, need to move to $DOTS ..."
+  if [ -d "$DOTS" ]
   then
-    echo Installing latest $PNAME: $RFILE
-    curl -L $PRURL --output ~/.dotfiles/$RFILE \
-      && sudo dpkg -i ~/.dotfiles/$RFILE \
-      && rm ~/.dotfiles/$RFILE \
-      && touch ~/.dotfiles/${PNAME}_${RFILE}.lock
+    echo " ... but $DOTS already exists, moving to backup"
+    mv "$DOTS" "$DOTS.bkp"
+  fi
+  mv "$CURR_DIR" "$HOME/.dotfiles"
+  echo " [OK]"
+fi
+
+
+# Installer Methods
+install_github_script() {
+  sh -c "$( curl -fsSL "https://raw.githubusercontent.com/$1/$2/$3" )"
+}
+install_github_debian() {
+  DEB_URL="$(
+    curl -fsSL "https://api.github.com/repos/$1/releases/latest" |\
+    jq -r '.assets[].browser_download_url' | grep deb
+  )"
+  PROJECT=${1##*/}
+  RELEASE=${DEB_URL##*/}
+  LOCKFILE="$DOTS/${PROJECT}_${RELEASE}.lock"
+  if [ ! -f "$LOCKFILE" ];
+  then
+    echo "Newer release found for $PROJECT: $RELEASE ..."
+    curl -fsSL "$DEB_URL" --output "$DOTS/$RELEASE" \
+      && sudo dpkg -i "$DOTS/$RELEASE" \
+      && rm "$DOTS/$RELEASE" \
+      && touch "$LOCKFILE"
   else
-    echo "$PNAME is already the newest version ($RFILE)."
+    echo "$PROJECT is already the newest version ($RELEASE)."
   fi
 }
-lrp_dpkg BurntSushi/ripgrep
-
-ltb () { # latest tag tarball
-  curl -L --silent "https://api.github.com/repos/$1/tags" | jq -r '.[0].tarball_url'
-}
-ltb_install () {
-  PNAME=${1##*/}
-  PRURL=$(ltb $1)
-  RFILE=${PRURL##*/}
-
-  if [ ! -f ~/.dotfiles/${PNAME}_${RFILE}.lock ];
+install_github_tartag() {
+  TAR_URL="$(
+    curl -fsSL "https://api.github.com/repos/$1/tags" |\
+    jq -r '.[0].tarball_url'
+  )"
+  PROJECT=${1##*/}
+  RELEASE=${TAR_URL##*/}
+  LOCKFILE="$DOTS/${PROJECT}_${RELEASE}.lock"
+  if [ ! -f "$LOCKFILE" ];
   then
-    echo Installing latest $PNAME: $RFILE
-    mkdir -p ~/.fzf \
-      && curl -L $PRURL | tar -xz -C ~/.$PNAME --strip-components=1 \
-      && yes | ~/.$PNAME/install \
-      && touch ~/.dotfiles/${PNAME}_${RFILE}.lock
+    echo "Newer release found for $PROJECT: $RELEASE ..."
+    mkdir -p "$HOME/.$PROJECT" \
+      && curl -fsSL "$TAR_URL" | tar -xz -C "$HOME/.$PROJECT" --strip-components=1 \
+      && yes | "$HOME/.$PROJECT/install" \
+      && touch "$LOCKFILE"
   else
-    echo "$PNAME is already the newest version ($RFILE)."
+    echo "$PROJECT is already the newest version ($RELEASE)."
   fi
 }
-ltb_install junegunn/fzf
 
+
+# Tools
+## ZSH
+sudo apt -qq install zsh
+install_github_script robbyrussell/oh-my-zsh master tools/install.sh
+
+## CLI goodies
+install_github_debian BurntSushi/ripgrep
+install_github_tartag junegunn/fzf
+
+# Symlinks
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-
