a60d89a6d62898a2874388f53d09a6d010265b17
Victor Villas <mail@victor.villas>
2021-12-21 11:54:52 -0300

    Change default Python installation to 3.8

--- install.sh
+++ install.sh
@@ -43,8 +43,8 @@ ln -sf "$DOTS/.vimrc"      "$HOME/.vimrc"
 export PATH="$HOME/.pyenv/bin:$PATH"
 if [ ! -d "$HOME/.pyenv" ]; then
   curl -fsSL https://pyenv.run | bash
-  pyenv install 3.7.12
-  pyenv global 3.7.12
+  pyenv install 3.8.12
+  pyenv global 3.8.12
   pip install --upgrade pip
 fi
 
